86c392772401e737dec2e88b9a4b70fe63c90eab
Aris Coronel <aris.coronel@ph.ibm.com>
2017-03-01 23:47:49 -0600

    my third commit

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 			</td>
 			<td>
 				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> This is great!!! My Bluemix Demo!! Thanks for creating a <span class="blue">PHP Starter Application</span>.
+				<p class='description'></p> Oh! This is great!!! My Bluemix Demo!! Thanks for creating a <span class="blue">PHP Starter Application</span>.
 			</td>
 		</tr>
 	</table>
